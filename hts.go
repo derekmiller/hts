167d636f0ade94196dfb6bee0daa186357471131
Derek Miller <dmiller@newrelic.com>
2020-01-15 22:59:09 -0800

    scrape html and marshal into showtime struct

--- hts.go
+++ hts.go
@@ -3,25 +3,41 @@ package main
 import (
 	"fmt"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gocolly/colly"
 )
 
+const hollywoodTheatreURL = "https://hollywoodtheatre.org/m/calendar/"
+
 type Showtime struct {
-	Title string
-	Date  string
-	Time  string
+	Series string
+	Title  string
+	Date   string
+	Time   string
 }
 
-func handler(request events.APIGatewayProxyRequest) error {
+func handler() error {
 	c := colly.NewCollector()
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+	c.OnHTML(".calendar__events__day", func(e *colly.HTMLElement) {
+		date := e.Attr("data-calendar-date")
+		e.ForEach(".calendar__events__day__event", func(_ int, e *colly.HTMLElement) {
+			series := e.ChildText(".calendar__events__day__event__series")
+			title := e.ChildText(".calendar__events__day__event__title")
+			e.ForEach(".showtime-square", func(_ int, e *colly.HTMLElement) {
+				time := e.ChildText(":first-child")
+				showtime := Showtime{
+					Series: series,
+					Title:  title,
+					Date:   date,
+					Time:   time,
+				}
+				fmt.Printf("Showtime: %v\n", showtime)
+			})
+		})
 	})
 
-	c.Visit("https://hollywoodtheatre.org/m/calendar/")
+	c.Visit(hollywoodTheatreURL)
 	return nil
 }
 
