a81c6a2f07b12349e3f13b85cb25390f2b2dc54f
Derek Miller <derek@Dereks-MacBook-Pro.local>
2020-02-06 19:24:20 -0800

    refactor along with formatting datetimes

--- go.mod
+++ go.mod
@@ -15,5 +15,5 @@ require (
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
-	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/appengine v1.6.5
 )
--- hts.go
+++ hts.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gocolly/colly"
+	"google.golang.org/appengine/log"
 )
 
 const hollywoodTheatreURL = "https://hollywoodtheatre.org/m/calendar/"
 
-type Showtime struct {
+type scrapedShowtime struct {
 	Series string
 	Title  string
 	Date   string
 	Time   string
 }
 
-func handler() error {
+type showtime struct {
+	Series        string
+	Title         string
+	StartDateTime time.Time
+}
+
+func handler(ctx context.Context) error {
+	scrapedShowtimes := scrapeShowtimes(hollywoodTheatreURL)
+
+	for _, s := range scrapedShowtimes {
+		if s.Title == "" {
+			log.Errorf(ctx, "no title found for scraped entry %v", s)
+			continue
+		}
+		if s.Date == "" || s.Time == "" {
+			log.Errorf(ctx, "no date and/or time for %v", s)
+			continue
+		}
+		parsedDateTime, err := parseDateTime(s.Date, s.Time)
+		if err != nil {
+			log.Errorf(ctx, "unable to parse datetime for %v on %v at %v: %v", s.Title, s.Date, s.Time, err)
+			continue
+		}
+		showtime := showtime{
+			Series:        s.Series,
+			Title:         s.Title,
+			StartDateTime: parsedDateTime,
+		}
+		fmt.Printf("Showtime: %v\n", showtime)
+	}
+	return nil
+}
+
+func scrapeShowtimes(url string) []scrapedShowtime {
+	var scrapedShowtimes []scrapedShowtime
 	c := colly.NewCollector()
 
 	c.OnHTML(".calendar__events__day", func(e *colly.HTMLElement) {
@@ -26,19 +63,27 @@ func handler() error {
 			title := e.ChildText(".calendar__events__day__event__title")
 			e.ForEach(".showtime-square", func(_ int, e *colly.HTMLElement) {
 				time := e.ChildText(":first-child")
-				showtime := Showtime{
+				scrapedShowtime := scrapedShowtime{
 					Series: series,
 					Title:  title,
 					Date:   date,
 					Time:   time,
 				}
-				fmt.Printf("Showtime: %v\n", showtime)
+				scrapedShowtimes = append(scrapedShowtimes, scrapedShowtime)
 			})
 		})
 	})
 
 	c.Visit(hollywoodTheatreURL)
-	return nil
+	return scrapedShowtimes
+}
+
+func parseDateTime(date, t string) (time.Time, error) {
+	dateTime := date + t
+	format := "2006-01-023:04 PM"
+	location, err := time.LoadLocation("America/Los_Angeles")
+	parsedDateTime, err := time.ParseInLocation(format, dateTime, location)
+	return parsedDateTime, err
 }
 
 func main() {
